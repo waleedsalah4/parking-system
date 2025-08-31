d548ec663cc9e73d2a4fa5a340b61f5bea2353b0
Waleed Salah <walidsalah652@gmail.com>
2025-08-31 03:25:41 +0300

    feat: add prettier and update config files to support path aliases

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import Home from "@/pages/Home";
+
 function App() {
   return (
     <main>
       <h1 className="text-4xl font-bold">Parking System</h1>
+      <Home />
     </main>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
